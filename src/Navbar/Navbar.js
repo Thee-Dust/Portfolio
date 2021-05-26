c26e0a611900281eaa9042f411c7d97aec377151
Dustin-Har <harbaughdustin@yahoo.com>
2021-05-25 20:55:03 -0400

    Add lnavbar links

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <nav>
-      
+      <div className='links'>
+        <Link to='/About'>About</Link>
+        <Link to='/Projects'>Projects</Link>
+        <Link to='/Resume'>Resume</Link>
+        <Link to='/Contact'>Contact Me</Link>
+      </div>
     </nav>
   )
 }
